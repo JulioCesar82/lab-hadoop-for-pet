6f33082cb26e32a386aa5f9e5f62e3d769cc29f1
Julio Ávila <julio.avila@mc1global.com>
2025-07-28 17:06:27 -0300

    Versãp 11.0 - Ajuste no Hadoop

--- resources/scripts/install_big_data_tools.sh
+++ resources/scripts/install_big_data_tools.sh
@@ -13,9 +13,10 @@ wget -q https://archive.apache.org/dist/hive/hive-${HIVE_VERSION}/apache-hive-${
     mv apache-hive-${HIVE_VERSION}-bin hive-${HIVE_VERSION}
 
 # Sqoop (1 MB)
-wget -q https://archive.apache.org/dist/sqoop/${SQOOP_VERSION}/sqoop-${SQOOP_VERSION}.tar.gz && \
-    tar -xf sqoop-${SQOOP_VERSION}.tar.gz >> /dev/null && \
-    rm sqoop-${SQOOP_VERSION}.tar.gz
+wget -q https://archive.apache.org/dist/sqoop/${SQOOP_VERSION}/sqoop-${SQOOP_VERSION}.bin__hadoop-2.6.0.tar.gz && \
+    tar -xf sqoop-${SQOOP_VERSION}.bin__hadoop-2.6.0.tar.gz >> /dev/null && \
+    rm sqoop-${SQOOP_VERSION}.bin__hadoop-2.6.0.tar.gz && \
+    mv sqoop-${SQOOP_VERSION}.bin__hadoop-2.6.0 sqoop-${SQOOP_VERSION}
 
 # HBase (210 MB)
 wget -q https://archive.apache.org/dist/hbase/${HBASE_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz && \
@@ -39,5 +40,8 @@ mkdir -p ${HIVE_HOME}/lib
 
 # Copy driver to all relevant lib directories
 cp postgresql-driver.jar ${SQOOP_HOME}/lib/
+
+cp ${SQOOP_HOME}/sqoop-${SQOOP_VERSION}.jar ${SQOOP_HOME}/lib/
+
 cp postgresql-driver.jar ${HIVE_HOME}/lib/
 cp postgresql-driver.jar ${HADOOP_HOME}/share/hadoop/common/lib/
