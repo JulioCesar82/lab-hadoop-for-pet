17da8d7f6fa12565e5d990415d1e952f72b7218d
Julio Ávila <julio.avila@mc1global.com>
2025-10-25 12:21:18 -0300

    Criado Scripts para execução do Hadoop

--- api/src/services/batch.service.js
+++ api/src/services/batch.service.js
@@ -1,4 +1,4 @@
-const pool = require('../config/database');
+const { pool } = require('../config/database');
 const { exec } = require('child_process');
 
 const startJob = async (jobName, command) => {
