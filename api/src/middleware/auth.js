56b3693e277778d21c71bae23d45310ad5e2415d
engsoft163 - Julio Cesar de Ávila <engsoft163@students.ic.unicamp.br>
2025-10-26 09:33:38 -0300

    Correção no auth.js

--- api/src/middleware/auth.js
+++ api/src/middleware/auth.js
@@ -1,22 +1,22 @@
 const organizationRepository = require('../repositories/postgres/organization.repository');
-const { UNAUTHORIZED, FORBIDDEN, INTERNAL_SERVER_ERROR } = require('../config/general');
+const { statusCodes } = require('../config/general');
 
 const authenticateApiKey = async (req, res, next) => {
     const apiKey = req.headers['x-api-key'];
     if (!apiKey) {
-        return res.status(UNAUTHORIZED).json({ message: 'API Key is required.' });
+        return res.status(statusCodes.UNAUTHORIZED).json({ message: 'API Key is required.' });
     }
 
     try {
         const organization = await organizationRepository.getOrganizationByApiKey(apiKey);
         if (!organization) {
-            return res.status(FORBIDDEN).json({ message: 'Invalid API Key.' });
+            return res.status(statusCodes.FORBIDDEN).json({ message: 'Invalid API Key.' });
         }
 
         if (organization.links && organization.links.length > 0) {
             const referer = req.headers.referer || req.headers.origin;
             if (!referer || !organization.links.some(link => referer.startsWith(link))) {
-                return res.status(FORBIDDEN).json({ message: 'Referer not allowed.' });
+                return res.status(statusCodes.FORBIDDEN).json({ message: 'Referer not allowed.' });
             }
         }
 
@@ -24,7 +24,7 @@ const authenticateApiKey = async (req, res, next) => {
         
         next();
     } catch (error) {
-        res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
+        res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
     }
 };
 
